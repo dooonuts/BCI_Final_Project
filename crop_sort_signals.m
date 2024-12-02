6c15f0e70af638c13e65ab7b171f2893555328aa
Daniel Wu <danielwu@wireless-10-154-102-150.public.utexas.edu>
2024-12-02 16:06:00 -0600

    added pca and testing one linear model

--- crop_sort_signals.m
+++ crop_sort_signals.m
@@ -1,7 +1,8 @@
-function [restMatrix,restTags,miMatrix,miTags] = crop_sort_signals(target_s,target_h)
+function [restMatrix,restTags,miMatrix,miTags] = crop_sort_signals(target_s,target_h, num_elements)
 
     %Usage: After doing sload, use this function with the resulting data
     %(target_s) and headers (target_h)
+    % num_elements = pad_size
 
     %Output: 
     %   restMatrix: cell matrix of however many rest trials were found in
@@ -12,6 +13,7 @@ function [restMatrix,restTags,miMatrix,miTags] = crop_sort_signals(target_s,targ
     %   the data (should also be 1x10)
     %   miTags: 1x10 matrix of the result of every MI trial (7702 for fail,
     %   7703 for success)
+   
 
 
     %769 is the trigger number for resting action
@@ -37,14 +39,20 @@ function [restMatrix,restTags,miMatrix,miTags] = crop_sort_signals(target_s,targ
     
             %Check the tag 2 indexes ahead, if it's 769 we have a right hand
             if(target_h.EVENT.TYP(n+2) == 769)
-                restMatrix{rest} = target_s(target_h.EVENT.POS(n):target_h.EVENT.POS(n+4),:);
+                tempMatrix = target_s(target_h.EVENT.POS(n):target_h.EVENT.POS(n+4),:);
+
+                restMatrix{rest} = padarray(tempMatrix, [num_elements - size(tempMatrix, 1),0], 0, 'post');
+
                 % restTags{rest} = target_h.EVENT.TYP(n+4);
                 temp =  target_h.EVENT.TYP(n+4);
-                restTags{rest} = floor(temp/10);
+
+                restTags{rest} = floor(temp/10); % Retrieves type regardless of pass/fail for online
                 rest = rest+1;
             %Or if it's 770 we have a left hand
             elseif(target_h.EVENT.TYP(n+2) == 770)
-                miMatrix{mi} = target_s(target_h.EVENT.POS(n):target_h.EVENT.POS(n+4),:);
+                tempMatrix = target_s(target_h.EVENT.POS(n):target_h.EVENT.POS(n+4),:);
+                miMatrix{mi} = padarray(tempMatrix, [num_elements - size(tempMatrix, 1),0], 0, 'post');
+
                 % miTags{mi} = target_h.EVENT.TYP(n+4);
                 temp = target_h.EVENT.TYP(n+4);
                 miTags{mi} = floor(temp/10);
--- project_test.m
+++ project_test.m
@@ -7,7 +7,9 @@ gdfFiles = gdfFiles(:);
 
 repetitions = ['r001';'r002';'r003';'r004'];
 sessions = ['s001';'s002';'s003'];
-subjects = ['107';'108';'109'];
+subjects = [107;108;109];
+
+num_elements = 10000;
 
 all_sessions = create_classes(gdfFiles);
 
@@ -15,9 +17,9 @@ all_sessions = create_classes(gdfFiles);
 MI_sessions = {};
 [~, num_sessions] = size(all_sessions);
 for i=1:num_sessions
-    if (convertCharsToStrings(all_sessions{i}.Type) == "MI")
+    if (convertCharsToStrings(all_sessions{i}.Type) == "MI" & str2num(all_sessions{i}.Subject) == 109)
         curr_session = all_sessions{i};
-        [pe_rest, pe_rest_spectrum, pe_rest_famp, rest_tags, pe_mi, pe_mi_spectrum, pe_mi_famp, mi_tags] = preprocess_session(curr_session.Filename);
+        [pe_rest, pe_rest_spectrum, pe_rest_famp, rest_tags, pe_mi, pe_mi_spectrum, pe_mi_famp, mi_tags] = preprocess_session(curr_session.Filename, num_elements);
         curr_session.PE_MI = pe_mi;
         curr_session.PE_MI_Spectrum = pe_mi_spectrum;
         curr_session.PE_MI_Famp = pe_mi_famp;
@@ -31,84 +33,98 @@ for i=1:num_sessions
     end
 end
 
-%% Testing Classifier
+%% Reshaping Data for PCA and Classification
 
 offline_mi_sessions = {};
 online_mi_sessions = {};
 
+num_elements = 10000;
+num_trials = 10;
+num_channels = 32;
+
 [~, num_mi_sessions] = size(MI_sessions);
 for i=1:num_mi_sessions
+    curr_session = MI_sessions{i};
     if(convertCharsToStrings(MI_sessions{i}.Online) == "Online")
-        curr_session = MI_sessions{i};
-        [~,num_trials] = size(curr_session.PE_MI_Famp);
-        curr_PE_MI_Famp = zeros(10,num_elements*32);
-        curr_PE_Rest_Famp = zeros(10,num_elements*32);
-        
-        for i=1:num_trials
-            temp_mi =  pca(abs(curr_session.PE_MI_Famp{i}));
-            temp_rest =  pca(abs(curr_session.PE_Rest_Famp{i}));
-            X_2D_mi = reshape(temp_mi, num_elements*32, [])';  % Transpose to make it N x M
-            X_2D_rest = reshape(temp_rest, num_elements*32, [])';  % Transpose to make it N x M
-            curr_PE_MI_Famp(i,:) = X_2D_mi;
-            curr_PE_Rest_Famp(i,:) = X_2D_rest;
-        end
-
-        % curr_session.PE_MI_Famp = curr_PE_MI_Famp;
-        % curr_session.PE_Rest_Famp = curr_PE_Rest_Famp;
-        online_mi_sessions{end+1} = curr_session; % 10x32x32
+        temp_session  = reshape_sessions(curr_session, num_elements, num_channels);
+        online_mi_sessions{end+1} = temp_session; % 10,(num_elements*num_channels)
     else
-        offline_mi_sessions{end+1} = MI_sessions{i};
+        temp_session  = reshape_sessions(curr_session, num_elements, num_channels);
+        offline_mi_sessions{end+1} = temp_session;
     end 
-
 end
 
-%%
+% Concatenate all sessions
+[~, num_online_sessions] = size(online_mi_sessions);
+[~, num_offline_sessions] = size(offline_mi_sessions);
 
-total_trials = vertcat(curr_PE_MI_Famp, curr_PE_Rest_Famp);
-total_tags = horzcat(cell2mat(online_mi_sessions{1}.MI_Tags), cell2mat(online_mi_sessions{1}.Rest_Tags));
+total_online_mi_famp= [];
+total_online_mi_tags = []; % becomes 120x1 without transpose with vertcat
+total_online_rest_famp = [];
+total_online_rest_tags = [];
 
-% train on offline, test on online
-% stuff = fitclinear(online_mi_sessions{1}.PE_MI_Famp, cell2mat(online_mi_sessions{1}.MI_Tags));
-% stuff = fitclinear(zeros(10,32), cell2mat(online_mi_sessions{1}.MI_Tags));
+total_offline_mi_famp= [];
+total_offline_mi_tags = []; % becomes 120x1 without transpose with vertcat
+total_offline_rest_famp = [];
+total_offline_rest_tags = [];
+ 
+% First cat offline 
+for i=1:num_offline_sessions
+    total_offline_mi_famp =  vertcat(total_offline_mi_famp,offline_mi_sessions{i}.PE_MI_Famp);
+    total_offline_mi_tags = vertcat(total_offline_mi_tags, cell2mat(offline_mi_sessions{i}.MI_Tags)'); 
 
-% Load or define EEG data
-% X: (num_samples x num_features) matrix of features (EEG data)
-% y: (num_samples x 1) vector of labels (1 or 2)
+    total_offline_rest_famp = vertcat(total_offline_rest_famp, offline_mi_sessions{i}.PE_Rest_Famp);
+    total_offline_rest_tags = vertcat(total_offline_rest_tags, cell2mat(offline_mi_sessions{i}.Rest_Tags)');
+end
 
-% Example initialization (replace this with your actual data)
-% X = randn(100, 64); % 100 trials, 64 features per trial
-% y = randi([1, 2], 100, 1); % Random labels (1 or 2)
+total_offline_sessions = vertcat(total_offline_mi_famp, total_offline_rest_famp); % Gives 80x320000;
+total_offline_tags = vertcat(total_offline_mi_tags, total_offline_rest_tags); % Gives 80x1;
 
-% % Split the data into training and testing sets
-% cv = cvpartition(size(X, 1), 'HoldOut', 0.3);
-% X_train = X(training(cv), :);
-% y_train = y(training(cv));
-% X_test = X(test(cv), :);
-% y_test = y(test(cv));
-% 
-% Train an LDA classifier
-lda_model = fitcdiscr(total_trials, total_tags);
+% Then cat online
+for i=1:num_online_sessions
+    total_online_mi_famp = vertcat(total_online_mi_famp,online_mi_sessions{i}.PE_MI_Famp);
+    total_online_mi_tags = vertcat(total_online_mi_tags, cell2mat(online_mi_sessions{i}.MI_Tags)');
+
+    total_online_rest_famp = vertcat(total_online_rest_famp, online_mi_sessions{i}.PE_Rest_Famp);
+    total_online_rest_tags = vertcat(total_online_rest_tags, cell2mat(online_mi_sessions{i}.Rest_Tags)');
+end
+
+total_online_sessions = vertcat(total_online_mi_famp, total_online_rest_famp); % Gives 160x320000;
+total_online_tags = vertcat(total_online_mi_tags, total_online_rest_tags); % Gives 160x1;
+
+total_sessions = vertcat(total_offline_sessions, total_online_sessions); % Should give 240x320000;
+total_tags = vertcat(total_offline_tags, total_online_tags); % Should give 240x1;
+
+total_sessions = total_sessions'; % Need to do this for PCA
+
+%% PCA
+% Should still be split  by trial so can separate later and then run
+% training, need to add real to make it not complex
+compressed_total_sessions = pca(real(total_sessions)); % Gives 240x240 - trials should still be second axis
+compressed_total_sessions = compressed_total_sessions'; % Gives 240x240 - trials should be in first axis - matching total tags
+
+%% Splitting Dataset And Splitting PCA Features
+% Get Num PCA Features
+num_features = 240; % Total is 240
+compressed_total_sessions_num_features = compressed_total_sessions(:,1:num_features);
+
+% Cross Fold/Split Data
+% 2x is because rest vs mi; Split Offline vs Online
+compressed_offline_sessions = compressed_total_sessions_num_features(1:2*num_offline_sessions*num_trials,:);
+compressed_online_sessions = compressed_total_sessions_num_features((2*num_offline_sessions*num_trials)+1:end,:);
+
+%% Training
+% Train an LDA classifier on offline data, test on online 
+lda_model = fitcdiscr(compressed_offline_sessions, total_offline_tags);
 
 % Predict on test data
-y_pred = predict(lda_model, total_trials)';
+y_pred = predict(lda_model, compressed_online_sessions);
 
 % Evaluate accuracy
-accuracy = sum(y_pred == total_tags) / length(total_tags);
+accuracy = sum(y_pred == total_online_tags) / length(total_online_tags);
 fprintf('Accuracy: %.2f%%\n', accuracy * 100);
 
-%% Testing 
-% first_trial = curr_session.PE_MI{1};
-% first_trial_spectrum = curr_session.PE_MI_Spectrum;
-% first_trial_famp = curr_session.PE_MI_Famp;
-% figure(1); clf;
-% plot(first_trial);
-% figure(2); clf;
-% plot(first_trial_spectrum{1},first_trial_famp{1});
-
-
-
 %% TODO LIST
-% Finish PCA
 % Create Model and Test
 % Linear Discriminant Analysis/Linear Regression
 % What happens if we train with first only session of top of that?
@@ -122,20 +138,17 @@ fprintf('Accuracy: %.2f%%\n', accuracy * 100);
 % 
 % dataTempEOG = dataTempFilt - EOG*b;
 
-%% PCA 
-% coeff = pca(dataSpaceTempFilt);
-
 %% Model 
 
 % Preprocess Each Session and Return, 
-function [pe_rest, pe_rest_spectrum, pe_rest_famp, rest_tags, pe_mi, pe_mi_spectrum, pe_mi_famp, mi_tags] = preprocess_session(curr_session_file)
+function [pe_rest, pe_rest_spectrum, pe_rest_famp, rest_tags, pe_mi, pe_mi_spectrum, pe_mi_famp, mi_tags] = preprocess_session(curr_session_file, num_elements)
 
     [s,h] = sload(curr_session_file);
     
     % Certain channels are unused:
     s = s(:,1:34);
 
-    [restMatrix,rest_tags,miMatrix,mi_tags] = crop_sort_signals(s,h);
+    [restMatrix,rest_tags,miMatrix,mi_tags] = crop_sort_signals(s,h, num_elements);
 
     [~, num_rest_trials] = size(restMatrix);
     [~, num_mi_trials] = size(miMatrix);
@@ -193,6 +206,34 @@ function [freqs, fft_shifted] = fft_with_shift(signal, sample_rate, axis)
     dt = 1/sample_rate; 
     df = 1/dt/(length(signal)-1); 
     freqs = -1/dt/2:df:1/dt/2; 
+end
+
+function [updated_session] = reshape_sessions(curr_session, num_elements, num_channels)
+    [~,num_trials] = size(curr_session.PE_MI_Famp);
+    updated_session = curr_session;
+    curr_PE_MI_Famp = zeros(num_trials,num_elements*32);
+    curr_PE_Rest_Famp = zeros(num_trials,num_elements*32);
+    
+    for i=1:num_trials
+        temp_mi =  curr_session.PE_MI_Famp{i};
+        temp_rest =  curr_session.PE_Rest_Famp{i};
+
+        padded_mi_floor = padarray(temp_mi, [floor((num_elements - size(temp_mi, 1))/2), 0], 0, 'pre');
+        padded_mi = padarray(padded_mi_floor, [ceil((num_elements - size(temp_mi, 1))/2), 0], 0, 'post');
+
+        padded_rest_floor = padarray(temp_rest, [floor((num_elements - size(temp_rest, 1))/2), 0], 0, 'pre');
+        padded_rest = padarray(padded_rest_floor, [ceil((num_elements - size(temp_rest, 1))/2), 0], 0, 'post');
+
+        % padded_mi = padarray(temp_mi, [num_elements - size(temp_mi, 1), num_channels - size(temp_mi, 2)], 0, 'post');
+        % padded_rest = padarray(temp_rest, [num_elements - size(temp_rest, 1), num_channels - size(temp_rest, 2)], 0, 'post');
+        X_2D_mi = reshape(padded_mi, num_elements*num_channels, [])';  % Transpose to make it N x M
+        X_2D_rest = reshape(padded_rest, num_elements*32, [])';  % Transpose to make it N x M
+        curr_PE_MI_Famp(i,:) = X_2D_mi;
+        curr_PE_Rest_Famp(i,:) = X_2D_rest;
+    end
+
+    updated_session.PE_MI_Famp = curr_PE_MI_Famp;
+    updated_session.PE_Rest_Famp = curr_PE_Rest_Famp;
 end
 
 function [freqs, reconstructed_signal] = ifft_with_shift(fft_data, sr)
